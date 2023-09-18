902d4cba2d82469e4c2dfa06e47ed83f0693898e
macljo <65873513+macljo@users.noreply.github.com>
2023-09-17 20:30:54 -0500

    Add project files.

---
+++ TTS/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TTS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OnClick1(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(userInput.Text, "What you entered", MessageBoxButton.OK, MessageBoxImage.Exclamation, MessageBoxResult.Yes);
+            var synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToWaveFile("output.wav");
+            //synthesizer.SetOutputToDefaultAudioDevice();
+            synthesizer.Speak(userInput.Text);
+        }
+    }
+}
